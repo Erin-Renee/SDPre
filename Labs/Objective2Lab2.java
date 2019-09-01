b2f1015eb3b7b7a62b82cd5562eac8a83894de1d
Erin Eustice <erinrenee.va@gmail.com>
2019-08-31 22:50:04 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,16 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name, state, one, two, three;
+    int age;
+
+    name = "Erin Eustice";
+    age = 31;
+    state = "Florida";
+    one = " name: ";
+    two = " age: ";
+    three = " state: ";
+
+    System.out.println( one + name + two + age + three + state );
+
+  }
+}

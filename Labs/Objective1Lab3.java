382497ce601a3e58073ea8c095ac3f6285f2ce44
Erin Eustice <erinrenee.va@gmail.com>
2019-08-31 19:36:03 -0500

    complete Objective 1 lab 2

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,10 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    System.out.print("Name: ");
+    System.out.println("Erin Eustice");
+    System.out.print("Age : ");
+    System.out.println("31");
+    System.out.print("State : ");
+    System.out.println("Florida");
+  }
+}

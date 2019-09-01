53d3ff3fb8742739bca896c9975ce20036aec75e
Erin Eustice <erinrenee.va@gmail.com>
2019-08-31 20:58:24 -0500

    complete Objective2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,8 @@
+public class Objective2Lab5 {public static void main(String[] args){
+    double A, B, hypotenuse;
+     A = (10 * 10);
+     B = (8 * 8);
+    hypotenuse = Math.sqrt(A + B);
+    System.out.println(hypotenuse);
+    }
+  }

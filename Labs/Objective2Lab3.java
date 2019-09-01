a71fbba7e259612239c489d21c14d49fc1d079a6
Erin Eustice <erinrenee.va@gmail.com>
2019-08-31 23:38:25 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,18 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3, first, second, third;
+
+    food1 = "Cherries";
+    food2 = "Chocolate";
+    food3 = "Bananas";
+    first = "1. ";
+    second = "2. ";
+    third = "3. ";
+
+    System.out.println( "My Favorite Foods Are:");
+    System.out.println( first + food1 );
+    System.out.println( second + food2 );
+    System.out.println( third + food3 );
+
+  }
+}

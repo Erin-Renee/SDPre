dfe9e16714a4177cac398910b87bf6c25f49ad35
Erin Eustice <erinrenee.va@gmail.com>
2019-10-27 13:27:39 -0500

    Ex 1 with study drills

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
-  public static void main(String[] args) {
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+  public static void main( String[] args ) {
+    System.out.println( "I, Erin Eustice, am determined to learn code." );
+    System.out.println( "Today's date is October 23, 2019" );
   }
 }

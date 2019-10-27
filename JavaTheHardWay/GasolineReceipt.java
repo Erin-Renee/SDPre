c72737f2f93912cbf65ca60ed7bc0d514d65b1b5
Erin Eustice <erinrenee.va@gmail.com>
2019-10-27 13:31:34 -0500

    ex 2 with study drills

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+    public static void main( String[] args ) {
+        System.out.println( "+-----------------------+" );
+        System.out.println( "|                       |" );
+        System.out.println( "|      CORNER STORE     |" );
+        System.out.println( "|                       |" );
+        System.out.println( "| 2019-10-23  04:38PM   |" );
+        System.out.println( "|                       |" );
+        System.out.println( "| Gallons:       12.870 |" );
+        System.out.println( "| Price/Gallon: $ 2.250 |" );
+        System.out.println( "|                       |" );
+        System.out.println( "| Fuel total:  $ 28.957 |" );
+        System.out.println( "|                       |" );
+        System.out.println( "+-----------------------+" );
+  }
+}

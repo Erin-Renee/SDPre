7f66502e883f9f9450fe660d1f89c6b9f8eafea7
Erin Eustice <erinrenee.va@gmail.com>
2019-10-27 13:41:14 -0500

    Ex 4 study drill #2

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,7 @@
+public class PrintingChoicesEscapes {
+  public static void main( String[] args ) {
+    System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nI am learning Java the Hard Way!\n\n" );
+  }
+}
+// Exercise 4: Escape Sequences and comments Study Drill #2
+// rewrite exercise 3 so that it has identitcal looking output but only uses a single println()

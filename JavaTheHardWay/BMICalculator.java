798e438ef4b395266c313810b8c4e5e75bfe21f9
Erin Eustice <erinrenee.va@gmail.com>
2019-10-30 22:44:22 -0600

    ex 9 study drill 2

--- JavaTheHardWay/BMICalculator.java
+++ JavaTheHardWay/BMICalculator.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class BMICalculator {
   public static void main( String[] args ) {
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, in, lbs, bmi, a, b;
+    double m, kg, lbs, bmi, ft, a, b, x, y, z, in;
+
+    a = 0.0254;
+    x = 12;
+    b = 2.20;
+
+    System.out.println( "How tall are you? Please enter your height in feet, then enter the inches separately." );
+    System.out.print( "your height in feet: " );
+    ft = keyboard.nextDouble();
 
     System.out.print( "your height in inches: " );
     in = keyboard.nextDouble();
-    a = 0.0254;
-    m = in*a;
+    y = (ft*x);
+    z = in + y;
+    m = z*a;
 
     System.out.print( "your weight in pounds: " );
     lbs = keyboard.nextDouble();
-    b = 2.20;
     kg = (lbs /b);
     bmi = kg / (m*m);
 

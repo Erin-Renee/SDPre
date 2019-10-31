f937d302d8016b8b9ba444af86d51adb21297e11
Erin Eustice <erinrenee.va@gmail.com>
2019-10-31 11:49:48 -0600

    Ex 25 Repeating Yourself with the While Loop

--- JavaTheHardWay/EnterPIN.java
+++ JavaTheHardWay/EnterPIN.java
@@ -11,7 +11,7 @@ public class EnterPIN {
     System.out.print("ENTER YOUR PIN: ");
     entry = keyboard.nextInt();
 
-    while ( entry != pin ) {
+    while ( entry != pin ) {                            //while loops!
       System.out.println("\nINCORRECT PIN. TRY AGAIN.");
       System.out.print("ENTER YOUR PIN: ");
       entry = keyboard.nextInt();
@@ -19,3 +19,12 @@ public class EnterPIN {
     System.out.println("\nPIN ACCEPTED. YOUR ACCOUNT BALANCE IS $425.17");
   }
 }
+
+/*
+while loops are similar to an if statement. THey both hava a condition in the
+parenthesis that is checked to see if it is true or false. If it is false, they both will skip all the code in the body.
+The difference is that if statements are true will execute all of the code in curly brackets only once.
+While loops will execute true statements then go back up to check it again over and over again until it eventually is false.
+The wile loop will then move on to the rest of the code.
+While loops prevent you from having to repeditively type out code....
+*/

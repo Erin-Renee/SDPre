da55bbbcc623749bfa69e668f62084c30bc2c433
Erin Eustice <erinrenee.va@gmail.com>
2019-11-01 08:13:01 -0600

    Infinite loops with study drill

--- JavaTheHardWay/KeepGuessing.java
+++ JavaTheHardWay/KeepGuessing.java
@@ -12,13 +12,23 @@ public class KeepGuessing {
     System.out.print( "Your guess: " );
     guess = keyboard.nextInt();
 
-    while ( secret != guess ) {   //program checks to see if secret != guess
-      System.out.println( "That is incorrect. Guess again." );
-      System.out.print( "Your guess: " ); //if secret !=guess the program executes the body
-    }
-//if secret == guess program will skip over loop to line 19
+    while ( secret != guess ) {   //program checks to see if secret != guess if it is it executes the body
+        if (guess != secret ) {
+            System.out.println( "That is incorrect. Guess again." );
+            System.out.print( "Your guess: " );
+            guess = keyboard.nextInt();   //user inputs next guess
+          }
+        }
+//if secret == guess program will skip over loop
+
     System.out.println( "That's right! You're a good guesser." );
   }
 }
 // This program has an infinite loop
 // CTRL-C to end the program.....
+
+/*
+ Study drill: Program needed if statement so it knows what to do if the variable
+ secret is not equal to the variable guess. line 19 then lets the user input
+ the next guess.
+ */

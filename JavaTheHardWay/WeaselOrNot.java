b743028befddb50104f59b9878ad0e6c18bb831f
Erin Eustice <erinrenee.va@gmail.com>
2019-11-06 06:47:46 -0700

    Ex 13 Comparing Strings with SD

--- JavaTheHardWay/WeaselOrNot.java
+++ JavaTheHardWay/WeaselOrNot.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WeaselOrNot {
+public class WeaselOrNot {  //ex 13 Comparing Strings
   public static void main( String[] args ) {
     Scanner keyboard = new Scanner(System.in);
     String word;
@@ -9,9 +9,16 @@ public class WeaselOrNot {
     System.out.println( "type the word \"weasel\", please." );
     word = keyboard.next();
 
-    yep = word.equals( "weasel");
+    yep = "weasel".equals( word );
     nope = ! word.equals("weasel");
     System.out.println( "You typed what was requested: " + yep );
     System.out.println( "You ignored polite instructions: " + nope );
   }
 }
+/*
+Study DRILL
+1.  changed the comparison on line 12 so that "weasel" is in front od the dot
+and equals is inside parenthesis. The code worked, and ran the same. I think
+this is because the equals() compares the string on the left to the one on the
+right or vice versa, it doesnt matter which side it is on.
+*/

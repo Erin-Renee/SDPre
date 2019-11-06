49b41bceaa834607bec38edc72e3f96f1851eb47
Erin Eustice <erinrenee.va@gmail.com>
2019-11-06 06:38:01 -0700

    Ex 33 with Study Drills

--- JavaTheHardWay/KeepGuessing.java
+++ JavaTheHardWay/KeepGuessing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KeepGuessing {
+public class KeepGuessing {   //ex 27 Infinite Loops
   public static void main( String[] args ) {
     Scanner keyboard = new Scanner(System.in);
     int secret, guess;
@@ -13,14 +13,11 @@ public class KeepGuessing {
     guess = keyboard.nextInt();
 
     while ( secret != guess ) {   //program checks to see if secret != guess if it is it executes the body
-        if (guess != secret ) {
             System.out.println( "That is incorrect. Guess again." );
             System.out.print( "Your guess: " );
             guess = keyboard.nextInt();   //user inputs next guess
           }
-        }
 //if secret == guess program will skip over loop
-
     System.out.println( "That's right! You're a good guesser." );
   }
 }
@@ -28,7 +25,12 @@ public class KeepGuessing {
 // CTRL-C to end the program.....
 
 /*
- Study drill: Program needed if statement so it knows what to do if the variable
- secret is not equal to the variable guess. line 19 then lets the user input
- the next guess.
- */
+ Study drill:
+ while loops will keep going back to check to see if the condition
+  in parenthesis is true or false. if it is true, it will execute the body and
+  go back to check again until its eventually false.
+
+  Why it became an infinite loop is because it didnt give the opportunity for
+  the person to input the next guess. If there is not another input from the keyboard (line 18),
+  then it will keep checking the last input from the keyboard.
+  */
---
+++ JavaTheHardWay/ThereAndBackAgain.java
@@ -0,0 +1,35 @@
+public class ThereAndBackAgain {    //Ex 33: Calling a Function
+  public static void main( String[] args ) {
+    System.out.println( "Here." );
+    erebor();
+    System.out.println( "Back first time." );
+    //erebor();
+    System.out.println( "Back second time." );
+  }                               //end of body "main"
+
+    public static void erebor() {   //function called erebor is defined
+      System.out.println( "There." );
+    }                             //end of body "erebor"
+}
+/*
+STUDY DRILLS
+1. what happens when you remove the parenthesis at the end of the first function
+ call on line 4?
+  the program will not compile. Without the parentesis it is no longer a function
+   and is a variable that is not defined.
+   2. When you take out the function on line 6 "erebor" it no longer prints "there".
+   The original program runs:
+   Here.
+   There.
+   back first time.
+   there.
+   back second time.
+
+   edited version runs:
+   Here.
+   There.
+   back first time. 
+   back second time.
+
+
+*/

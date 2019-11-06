6ce3787d2f54001291812f9795d71a08f3b56e83
Erin Eustice <erinrenee.va@gmail.com>
2019-11-06 06:46:32 -0700

    Ex 12 Boolean Expressions with SD

--- JavaTheHardWay/BooleanExpressions.java
+++ JavaTheHardWay/BooleanExpressions.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BooleanExpressions {
+public class BooleanExpressions {   //Ex 12 Boolean Expressions pg 57
   public static void main( String[] args ) {
     Scanner keyboard = new Scanner(System.in);
     boolean a, b, c, d, e, f;
@@ -18,19 +18,30 @@ public class BooleanExpressions {
     e = (x > y);
     f = (x >= y);
 
-    System.out.println( x + " is LESS THAN " + y + ": " + a );
-    System.out.println( x + " is LESS THAN / EQUAL TO " + y + ": " + b );
-    System.out.println( x + " is EQUAL TO " + y + ": " + c );
-    System.out.println( x + " is NOT EQUAL TO " + y + ": " + d );
-    System.out.println( x + " is GREATER THAN " + y + ": " + e );
-    System.out.println( x + " is GREATER THAN / EQUAL TO " + y + ": " + f );
+    System.out.println( x + " is LESS THAN " + y + ": " + a );    //true
+    System.out.println( x + " is LESS THAN / EQUAL TO " + y + ": " + b ); //true
+    System.out.println( x + " is EQUAL TO " + y + ": " + c );   //false
+    System.out.println( x + " is NOT EQUAL TO " + y + ": " + d );   //true
+    System.out.println( x + " is GREATER THAN " + y + ": " + e ); //false
+    System.out.println( x + " is GREATER THAN / EQUAL TO " + y + ": " + f );  //false
     System.out.println();
 
-    System.out.println( !(x < y) + " " + (x >= y) );
-    System.out.println( !(x <= y) + " " + (x > y) );
-    System.out.println( !(x == y) + " " + (x != y) );
-    System.out.println( !(x != y) + " " + (x == y) );
-    System.out.println( !(x > y) + " " + (x <= y) );
-    System.out.println( !(x >= y) + " " + (x < y) );
+    System.out.println( !(x < y) + " " + (x >= y) ); //false false    // "not" operator is !   takes the logical OPPOSITE
+    System.out.println( !(x <= y) + " " + (x > y) );  //false false
+    System.out.println( !(x == y) + " " + (x != y) ); //true true
+    System.out.println( !(x != y) + " " + (x == y) ); // false false
+    System.out.println( !(x > y) + " " + (x <= y) );  //true true
+    System.out.println( !(x >= y) + " " + (x < y) ); //true true
   }
 }
+
+/*
+STUDY DRILL
+1.
+less than is <
+greater than is >
+less than or equal to is <=
+greater than or equal to is >=
+equal to is ==
+not equal to is !=
+*/
